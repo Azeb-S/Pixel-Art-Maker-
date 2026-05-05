031fff6c2fd79944e8cfccc46a887fdb05d453d5
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-05-05 11:43:47 -0700

    started the  data structure

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+const GRID_SIZE = 16
+const DEFAULT_COLOR = '#ffffff' // white unpainted
+
+function makeEmptyGrid(){
+    return Array.from(
+        {length: GRID_SIZE },
+        () => Array(GRID_SIZE).fill(DEFAULT_COLOR)
+    )
+}
+
+function App() {
+    const [grid, SetGrid] = useState(makeEmptyGrid)
+
+    const [currentColor, setCurrentColor] = useState('#1a1a1a')
+
+    return <div></div>
+}
+
+export default App
