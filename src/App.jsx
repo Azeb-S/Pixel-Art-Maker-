3dbf2182bd085591eaae6a10f1f179f72c321d54
Azeb Tesfay <azbseb@gmail.com>
2026-05-05 11:58:11 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,43 @@ import './App.css'
 const GRID_SIZE = 16
 const DEFAULT_COLOR = '#ffffff' // white unpainted
 
-function makeEmptyGrid(){
-    return Array.from(
-        {length: GRID_SIZE },
-        () => Array(GRID_SIZE).fill(DEFAULT_COLOR)
-    )
+function makeEmptyGrid() {
+  return Array.from(
+    { length: GRID_SIZE },
+    () => Array(GRID_SIZE).fill(DEFAULT_COLOR)
+  )
 }
 
 function App() {
-    const [grid, SetGrid] = useState(makeEmptyGrid)
+  const [grid, SetGrid] = useState(makeEmptyGrid)
 
-    const [currentColor, setCurrentColor] = useState('#1a1a1a')
+  const [currentColor, setCurrentColor] = useState('#1a1a1a')
 
-    return <div></div>
+  return (<div className='pixel-art'>
+    <h1>Pixel Art Editor</h1>
+
+    <div className='pixel-grid'
+      style={{ gridTemplateColumns: `repeat(${GRID_SIZE}, 1fr)` }}
+    >
+
+      {grid.map((row, r) =>
+        row.map((color, c) => (
+          <button
+            key={`${r}-${c}`}
+            className='pixel'
+            style={{ background: color }}
+            areal-label={`Pixel ${r}, ${c}`}
+          />
+        ))
+      )}
+
+
+    </div>
+
+
+
+  </div>
+  )
 }
 
 export default App
